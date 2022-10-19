d236bc92ecb08a18a49ed89bd9606bb9c373b0eb
cheruto-yego <107578924+cheruto-yego@users.noreply.github.com>
2022-10-19 18:49:22 +0300

    Add files via upload

---
+++ no_7.py
@@ -0,0 +1,33 @@
+# birth_year=2002
+# def calculateAge(birth_year,name,weight):
+#     today=2022
+#     age=today-birth_year
+#     print("your age is:",age)
+#     return age
+
+# name = str(input("What is your name"))
+# if name :#adds Lamara as default name
+#      name='yego'
+#     weight = int(input("What is your weight"))
+#     print(calculateAge(birth_year,name,weight))
+#     print("your name is",name,"your weight is",weight)
+#     #calculateAge(birth_year,name,weight)
+
+year=2020
+default='yego'
+def roman_details():
+    # [name,birth,weight]
+    name=input('what is your name?')
+
+    birth=int(input('what is your birth year'))
+    weight=int(input('what is your weight? '))
+    if birth==2002:
+        
+            
+            print('your name is ',name)
+            print('your birth year is  ',birth)
+            print('your weight is ',weight)
+birth=int(input('what is your birth year'))
+age =(year - birth )
+print('your age ', age )
+roman_details()
